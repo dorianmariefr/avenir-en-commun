505b261edc588c3cdedd8ae95dbc3a5379672bc3
Damien <damien@dam.io>
2021-01-26 12:47:43 +0000

    store content in db and changes things related to that

--- core/models.py
+++ core/models.py
@@ -3,11 +3,13 @@ from django.db import models
 class Chapter(models.Model):
     number = models.CharField(max_length=500, primary_key=True)
     slug = models.CharField(max_length=500)
+    title = models.CharField(max_length=500)
     content = models.TextField()
 
 
 class Article(models.Model):
     number = models.CharField(max_length=500, primary_key=True)
     slug = models.CharField(max_length=500)
+    title = models.CharField(max_length=500)
     content = models.TextField()
-    chapter = models.ForeignKey(Chapter)
+    chapter = models.ForeignKey(Chapter, on_delete=models.DO_NOTHING)
--- core/views.py
+++ core/views.py
@@ -1,8 +1,7 @@
 from pathlib import Path
 from django.shortcuts import render
 
-import markdown
-import glob
+from .models import Chapter, Article
 
 
 def home(request):
@@ -10,25 +9,20 @@ def home(request):
 
 
 def toc(request):
-    return render(request, "toc.html")
+    return render(request, "toc.html",{
+        'chapters': Chapter.objects.all()
+    })
 
 
 def chapter(request, n, slug):
-    base_dir = Path(__file__).resolve().parent.parent
-    file_dir = base_dir / 'programme' / f'chapitre-{n}' / 'index.md'
+    chapter = Chapter.objects.get(number=n)
     return render(request, "chapter.html", {
-        'content': markdown.Markdown().convert(open(file_dir).read())
+        'chapter': chapter
     })
 
 
 def section(request, n, slug):
-    base_dir = Path(__file__).resolve().parent.parent
-
-    file_dir = None
-    for file in glob.glob(f"{base_dir / 'programme'}/**", recursive=True):
-        if f"/{n}.md" in file:
-            file_dir = file
-            break
+    article = Article.objects.get(number=n)
     return render(request, "section.html", {
-        'content': markdown.Markdown().convert(open(file_dir).read())
+        'article': article
     })
